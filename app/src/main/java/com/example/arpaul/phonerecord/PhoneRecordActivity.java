dc33f233c71d9f2413d1581f608ee0b1debc5298
Aritra1704 <aritra1704@gmail.com>
2016-03-08 22:37:49 +0530

    Type: Creating PhoneRecord Application.
    Body: Creating the project from the beginning.
    
     Footer:

---
+++ app/src/main/java/com/example/arpaul/phonerecord/PhoneRecordActivity.java
@@ -0,0 +1,13 @@
+package com.example.arpaul.phonerecord;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_record);
+    }
+}
